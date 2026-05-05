40885f37c61f22f146832bc7eff49da4f271f64b
Utpala9 <utpaladutta69@gmail.com>
2026-05-05 16:30:09 +0530

    employee

--- apr30_26/Employee.java
+++ apr30_26/Employee.java
@@ -1,17 +1,57 @@
 package apr30_26;
 
 public class Employee {
-    string name ;
-    string department ;
-    int id ;
+	int id;
+	String name;
+	
+//	Employee(int id,String name){
+//		this.id=id;
+//		this.name=name;
+//	}
+	
+	void dispalyInfo() {
+		System.out.println(this.id + " " + this.name);
+	}
+	
 }
 
-public static Developer( string name, string department int id, string programmingLanguage){
-    super(name, department, id);
-    this.programmingLanguage = programmingLanguage;         
-}
-public static Manager( string name, string department int id, int teamSize){
-    super(name, department, id);
-    this.teamSize = teamSize;         
+
+
+
+public class Developer extends Employee {
+	String role;
+	
+	Developer(int id,String name,String role){
+		this.id=id;
+		this.name=name;
+		this.role=role;
+	}
+	void displayInfo() {
+		System.out.println(this.id + " " + this.name + " " + this.role);
+	}
 }
 
+
+
+public class Manager extends Employee {
+	double salary;
+	Manager(int id,String name,double salary){
+		this.id=id;
+		this.name = name;
+		this.salary = salary;
+	}
+	void dispalyInfo() {
+		System.out.println(this.id + " " + this.name + " " + this.salary);
+	}
+}
+
+
+
+public class Test {
+	public static void main(String[] args) {
+		Developer d = new Developer(20,"surya","Sql Developer");
+		d.displayInfo();
+		Manager m = new Manager(25,"Sai",25000);
+		m.dispalyInfo();
+	}
+}
